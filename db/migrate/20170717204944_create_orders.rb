40aed1e0bad0f77887378d6b4584da84fe6fe0f4
Richard Casanova <ricardofer2412@hotmail.com>
2017-07-21 00:30:42 -0400

    First Commit

---
+++ db/migrate/20170717204944_create_orders.rb
@@ -0,0 +1,16 @@
+class CreateOrders < ActiveRecord::Migration[5.1]
+  def change
+    create_table :orders do |t|
+      t.string :custumer_name
+      t.integer :ponumber
+      t.string :payable
+      t.string :status
+      t.string :terms
+
+
+
+      t.timestamps
+    end
+
+  end
+end
